931b01bfefa3e462e26a3728b390682a403d9c11
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-12-03 11:24:19 +0100

    Animacoes explicitas construidas

---
+++ lib/animacoes_explicitas_construidas.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+class AnimacoesExplicitasConstruidas extends StatefulWidget {
+  const AnimacoesExplicitasConstruidas({super.key});
+
+  @override
+  State<AnimacoesExplicitasConstruidas> createState() => _AnimacoesExplicitasConstruidasState();
+}
+
+class _AnimacoesExplicitasConstruidasState extends State<AnimacoesExplicitasConstruidas> with SingleTickerProviderStateMixin{
+
+ late AnimationController _animationController;
+
+  AnimationStatus? _animationStatus;
+
+ late Animation<double> _animation;
+
+ @override
+ void initState() {
+   super.initState();
+   _animationController = AnimationController(
+     duration: Duration(seconds: 10),
+     vsync: this
+   )..repeat()..addStatusListener((status) { 
+       _animationStatus = status;
+   });
+
+   _animation = CurvedAnimation(
+      parent: _animationController, 
+      curve: Curves.easeInOut);
+   
+ }
+
+ @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  
+  @override
+  Widget build(BuildContext context) { 
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            width: 500,
+            height: 400,
+            child: RotationTransition(
+              alignment: Alignment.center,
+              child: Image.asset("imagem/unitel.webp"),
+              turns: _animation
+              ),
+          ),
+          TextButton(
+            onPressed: (){
+              
+              if( _animationStatus == AnimationStatus.dismissed){
+                 
+                 _animationController.forward();
+
+              }
+              else{
+
+                _animationController.reverse();
+
+              }
+              
+
+            }, 
+            child: Text("Presione", 
+              style: TextStyle(
+                color: Colors.black
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:animacao/animacao_implicita.dart';
 import 'package:animacao/animacao_tween.dart';
-import 'package:animacao/criando_animacoes_basicas.dart';
+import 'package:animacao/animacoes_explicitas_construidas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-     home: AnimacaoTween(),
+     home: AnimacoesExplicitasConstruidas(),
   ));
 }
 
